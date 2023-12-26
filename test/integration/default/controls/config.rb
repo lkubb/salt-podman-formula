46bd66e94b3da54bbd4275228b37eab526c1980d
jeanluc <lkubb@protonmail.com>
2023-12-26 15:27:09 +0100

    chore: Update template

--- test/integration/default/controls/config.rb
+++ test/integration/default/controls/config.rb
@@ -24,10 +24,10 @@ control 'podman.config.file' do
       # rubocop:disable Lint/RedundantCopDisableDirective
       # rubocop:disable Layout/LineLength
       should include(
-        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", '\
-        '"roles", "osfinger", "os", "os_family"], "source_files": '\
-        '{"podman-config-file-file-managed": ["example.tmpl.jinja"], '\
-        '"podman--config-file-file-managed": '\
+        '"tofs": {"files_switch": ["any/path/can/be/used/here", "id", ' \
+        '"roles", "osfinger", "os", "os_family"], "source_files": ' \
+        '{"podman-config-file-file-managed": ["example.tmpl.jinja"], ' \
+        '"podman--config-file-file-managed": ' \
         '["-example.tmpl.jinja"]}'
       )
       # rubocop:enable Layout/LineLength
